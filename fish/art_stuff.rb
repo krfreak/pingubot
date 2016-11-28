b60718601f142d9a904aea4dd21c6a9beeaa5f3e
Tim <kreedzfreak@gmail.com>
2016-11-28 21:54:23 +0100

    Update art_stuff.rb

--- fish/art_stuff.rb
+++ fish/art_stuff.rb
@@ -5,57 +5,32 @@ module Bot
       puts 'Art loaded'
 
       command :art, description: "Get a randomly chosen artwork that contains <tag1>\n Usage: !!art <tag1> <-tag2>\n- excludes that tag from the request" do |event, *args|
-        inc = []
-        exc = []
-        unless args.blank?
-          inc = Marshal.load(Marshal.dump(args))
-          inc.each(&:downcase!)
-          inc.each do |a|
-            exc.push a if a.chars.first == '-'
-          end
-          inc.delete_if do |a|
-            a.start_with? '-'
-          end
-          inc.delete_if do |a|
-            a.start_with? '+'
-          end
-          exc.each do |a|
-            a.slice!(0)
-          end
-        end
-        unless args.include? '+nsfw'
-          exc.push 'nsfw' unless args.include? 'nsfw'
-        end
-        result = get_art(inc, exc, ((args.include? 'nsfw') || (args.include? '+nsfw')))
+        result = get_art parse_input args
         if !result.nil?
-          if (args.include? 'nsfw') || (args.include? '+nsfw')
-            "Random #{args.to_s.downcase} art (#{result[1]} results): <#{result[0]}>"
-          else
-            "Random #{args.to_s.downcase} art (#{result[1]} results): #{result[0]}"
-          end
+          random_text result
         else
           'Sorry, got nothing on that.'
         end
       end
 
       command :ca, description: "Get a random art with Chloe" do
-        random_text "Chloe"
+        random_text get_art parse_input ["chloe"]
       end
 
       command :pfa, description: "Get a random art with Pricefield" do
-        random_text "Pricefield"
+        random_text get_art parse_input ["pricefield"]
       end
 
       command :ka, description: "Get a random art with Kate" do
-        random_text "Kate"
+        random_text get_art parse_input ["kate"]
       end
 
       command :cua, description: "Get a random cute art" do
-        random_text "cute"
+        random_text get_art parse_input ["cute"]
       end
 
       command :ra, description: "Get a random art with Rachel" do
-        random_text "rachel"
+        random_text get_art parse_input ["rachel"]
       end
 
     end
@@ -69,22 +44,47 @@ class Art
   field :tags,      type: Array
   field :preview,   type: String
   field :source,    type: String
-  field :last_use,  type: Date
+  field :last_use,  type: DateTime
 end
 
-def get_art(inc, exc, nsfw = false)
-  exc << "nsfw" unless nsfw
-  art = inc.blank? ? Art.where(tags: { '$nin' => exc}) : Art.where(tags: { '$all' => inc }).and(tags: { '$nin' => exc })
+def get_art(tags)
+  art = tags[:inc].blank? ? Art.where(tags: { '$nin' => tags[:exc]}) : Art.where(tags: { '$all' => tags[:inc] }).and(tags: { '$nin' => tags[:exc] })
   count = art.count
   art = art.order_by(last_use: :desc).last
   if art.present?
-    art.touch :last_use
-    [art[:link], count]
+    art.update(:last_use => DateTime.now)
+    {:art => art, :args => tags[:raw_input], :count => count}
   else
     nil
   end
 end
 
-def random_text(text)
-  "Random #{text} art: " << get_art([text.downcase], [])[0]
+def parse_input(input)
+  inc = []
+  exc = []
+  unless input.blank?
+    inc = Marshal.load(Marshal.dump(input))
+    inc.each(&:downcase!)
+    inc.each do |a|
+      exc.push a if a.chars.first == '-'
+    end
+    inc.delete_if do |a|
+      a.start_with? '-'
+    end
+    inc.delete_if do |a|
+      a.start_with? '+'
+    end
+    exc.each do |a|
+      a.slice!(0)
+    end
+  end
+  unless input.include? '+nsfw'
+    exc.push 'nsfw' unless input.include? 'nsfw'
+  end
+  {:inc => inc, :exc => exc, :raw_input => input}
+end
+
+def random_text(art)
+  p art
+  "Random #{art[:args]} art (#{art[:count]} results): " << (art[:art].tags.include?("nsfw") ? "[NSFW] <#{art[:art].link}>" : "#{art[:art].link}")
 end

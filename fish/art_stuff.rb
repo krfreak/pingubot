5a8e4edfa7aeb18d82f1219ddda768d8a2f13fe4
Tim Schnell <kreedzfreak@gmail.com>
2017-05-14 22:40:51 +0200

    Update art_stuff.rb

--- fish/art_stuff.rb
+++ fish/art_stuff.rb
@@ -32,6 +32,78 @@ module Bot
         output "rachel", event
       end
 
+      protected
+
+      def ArtStuff::output(args, event=nil)
+        args = [] << args unless args.kind_of?(Array)
+        result = get_art parse_input args
+        if !result.nil?
+          begin
+            raise Discordrb::Errors::NoPermission if event.nil?
+            event.channel.send_embed do |embed|
+              embed.title = "Artist page"
+              embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Random #{result[:args]} art")
+              embed.colour = 0x14506f
+              if result[:art].tags.include?("nsfw")
+                embed.description = "**[NSFW]**"
+              else
+                embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{result[:art].preview}")
+              end
+              embed.url = "#{result[:art].source}"
+              embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{result[:art].tags.include?("nsfw") ? "**[NSFW]**, " : ""}#{result[:count]} results")
+              embed.add_field(name: "Tags:", value: "```#{result[:art].tags.reject(&:empty?).join(', ')}```")
+            end
+          rescue Discordrb::Errors::NoPermission
+            random_text result
+          end
+        else
+          'Sorry, got nothing on that.'
+        end
+      end
+
+      def ArtStuff::get_art(tags)
+        art = tags[:inc].blank? ? Art.where(tags: { '$nin' => tags[:exc]}) : Art.where(tags: { '$all' => tags[:inc] }).and(tags: { '$nin' => tags[:exc] })
+        count = art.count
+        art = art.order_by(last_use: :desc)
+        art = count>11 ? art[(count-(count*0.1).ceil)..-1].sample : art.last
+        if art.present?
+          art.update(:last_use => DateTime.now)
+          {:art => art, :args => tags[:raw_input], :count => count}
+        else
+          nil
+        end
+      end
+
+      def ArtStuff::parse_input(input)
+        inc = []
+        exc = []
+        unless input.blank?
+          inc = Marshal.load(Marshal.dump(input))
+          inc.each(&:downcase!)
+          inc.each do |a|
+            exc.push a if a.chars.first == '-'
+          end
+          inc.delete_if do |a|
+            a.start_with? '-'
+          end
+          inc.delete_if do |a|
+            a.start_with? '+'
+          end
+          exc.each do |a|
+            a.slice!(0)
+          end
+        end
+        unless input.include? '+nsfw'
+          exc.push 'nsfw' unless input.include? 'nsfw'
+        end
+        {:inc => inc, :exc => exc, :raw_input => input}
+      end
+
+      def ArtStuff::random_text(art)
+        "Random #{art[:args]} art (#{art[:count]} results): " << (art[:art].tags.include?("nsfw") ? "[NSFW] <#{art[:art].link}>" : "#{art[:art].link}")
+      end
+
+
     end
   end
 end
@@ -44,73 +116,4 @@ class Art
   field :preview,   type: String
   field :source,    type: String
   field :last_use,  type: DateTime
-end
-
-def output(args, event=nil)
-  args = [] << args unless args.kind_of?(Array)
-  result = get_art parse_input args
-  if !result.nil?
-    begin
-      raise Discordrb::Errors::NoPermission if event.nil?
-      event.channel.send_embed do |embed|
-        embed.title = "Artist page"
-        embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Random #{result[:args]} art")
-        embed.colour = 0x14506f
-        if result[:art].tags.include?("nsfw")
-          embed.description = "**[NSFW]**"
-        else
-          embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{result[:art].preview}")
-        end
-        embed.url = "#{result[:art].source}"
-        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{result[:art].tags.include?("nsfw") ? "**[NSFW]**, " : ""}#{result[:count]} results")
-        embed.add_field(name: "Tags:", value: "```#{result[:art].tags.reject(&:empty?).join(', ')}```")
-      end
-    rescue Discordrb::Errors::NoPermission
-      random_text result
-    end
-  else
-    'Sorry, got nothing on that.'
-  end
-end
-
-def get_art(tags)
-  art = tags[:inc].blank? ? Art.where(tags: { '$nin' => tags[:exc]}) : Art.where(tags: { '$all' => tags[:inc] }).and(tags: { '$nin' => tags[:exc] })
-  count = art.count
-  art = art.order_by(last_use: :desc)
-  art = count>11 ? art[(count-(count*0.1).ceil)..-1].sample : art.last
-  if art.present?
-    art.update(:last_use => DateTime.now)
-    {:art => art, :args => tags[:raw_input], :count => count}
-  else
-    nil
-  end
-end
-
-def parse_input(input)
-  inc = []
-  exc = []
-  unless input.blank?
-    inc = Marshal.load(Marshal.dump(input))
-    inc.each(&:downcase!)
-    inc.each do |a|
-      exc.push a if a.chars.first == '-'
-    end
-    inc.delete_if do |a|
-      a.start_with? '-'
-    end
-    inc.delete_if do |a|
-      a.start_with? '+'
-    end
-    exc.each do |a|
-      a.slice!(0)
-    end
-  end
-  unless input.include? '+nsfw'
-    exc.push 'nsfw' unless input.include? 'nsfw'
-  end
-  {:inc => inc, :exc => exc, :raw_input => input}
-end
-
-def random_text(art)
-  "Random #{art[:args]} art (#{art[:count]} results): " << (art[:art].tags.include?("nsfw") ? "[NSFW] <#{art[:art].link}>" : "#{art[:art].link}")
 end
